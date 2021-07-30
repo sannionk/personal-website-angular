845ab90968ac85ae607fa7d57811dfc0b18649fa
Sanni <onkalosanni@gmail.com>
2021-07-30 21:26:33 +0300

    modify server

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
-app.use(express.static('./dist/personal-website'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/personal-website'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/personal-website/'}),
-);
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/personal-website/index.html'));
+});
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
